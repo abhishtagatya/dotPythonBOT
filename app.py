bdb031127220f88e372f9cc2d2734b4f4e3b9c5f
Abhishta Gatya <abhishtagatya@yahoo.com>
2017-12-23 23:25:59 +0700

    added help and echo

--- app.py
+++ app.py
@@ -51,9 +51,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+
+    if event.message.text != 'help':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
+    else :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Need Help?')
+        )
+
 
 
 if __name__ == "__main__":
