7296cd44ae7026ea73848671369b4577d8484461
Abhishta Gatya <abhishtagatya@yahoo.com>
2017-12-24 17:57:04 +0700

    revert

--- app.py
+++ app.py
@@ -15,8 +15,6 @@
 import os
 import sys
 
-import src.test
-
 from flask import Flask, request, abort
 
 from linebot import (
@@ -56,7 +54,17 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    src.test.test_message(event)
+    if event.message.text != 'help':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
+    else :
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Need Help?')
+        )
 
 
 
