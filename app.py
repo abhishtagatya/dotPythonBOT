302c749351791e09f46fbd2f9c0755f092c19d17
Abhishta Gatya <abhishtagatya@yahoo.com>
2017-12-24 18:02:51 +0700

    echo Class

--- app.py
+++ app.py
@@ -53,18 +53,23 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    message_echo(event)
 
-    if event.message.text != 'help':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
 
-    else :
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='Need Help?')
-        )
+class BotReply:
+
+    def message_echo(event):
+        if event.message.text != 'help':
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=event.message.text)
+            )
+
+        else :
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='Need Help?')
+            )
 
 
 
