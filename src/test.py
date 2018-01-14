2d1abb419fb51c35e92126dbe4ed8bbfe1c7777e
Abhishta Gatya <abhishtagatya@yahoo.com>
2018-01-14 12:34:16 +0700

    v0.94 PRE-RELEASE TEST #8

--- src/test.py
+++ src/test.py
@@ -3,4 +3,4 @@ import json
 
 with open('src/course/basic_challenge.json','r') as coord:
     gv = json.load(coord)
-    print(gv['challenge']['syntax'][list(random.choice(gv['challenge']['syntax'].keys()))])
+    print
